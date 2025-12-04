12b1aead059c58ff7b66f978ced07701456f5d3a
nichole <nichole@climbwithyourfeet.com>
2025-12-04 15:49:46 -0800

    updating test data with corrected data

--- src/test/python/movie_lens_retrieval/test_RetrieverAndRander.py
+++ src/test/python/movie_lens_retrieval/test_RetrieverAndRander.py
@@ -1,16 +1,11 @@
 import os.path
 import unittest
 
-import tensorflow as tf
 from helper import *
 from movie_lens_retrieval.RetrieverAndRanker import RetrieverAndRanker
 
 class TestRetrievalAndRanker(unittest.TestCase):
   def setUp(self):
-    
-    self.joined_ratings_tf_records = [os.path.join(get_project_dir(),
-      "src/test/resources/data/sorted_1/tfrecord-*.gz"),
-      os.path.join(get_project_dir(),"src/test/resources/data/sorted_2/tfrecord*.gz"),]
     
     saved_models_dir = os.path.join(get_project_dir(), "src/main/resources/serving_models")
     self.user_movie_models_dir = os.path.join(saved_models_dir, "user_movie_model")
@@ -32,7 +27,7 @@ class TestRetrievalAndRanker(unittest.TestCase):
       "genres" : tf.io.FixedLenFeature([], tf.string)}
     self.max_k = 10
       
-  def _est_indexer_tensors(self):
+  def test_indexer_tensors(self):
    
     loaded_model = tf.saved_model.load(self.user_movie_models_dir)
     
@@ -58,35 +53,13 @@ class TestRetrievalAndRanker(unittest.TestCase):
       a = set([i  for _list in neighbor_idxs for i in _list])
       self.assertTrue(0 in a)
       self.assertTrue(1 in a)
-    
-  def _est_bloom_filters(self):
-    
-    loaded_model = tf.saved_model.load(self.user_movie_models_dir)
-    
-    user_indexers, user_ids = RetrieverAndRanker._create_user_indexers(self.user_inputs,
-      loaded_model, self.feature_spec, self.max_k)
-    
-    ratings_ds = RetrieverAndRanker._joined_ratings_tt_to_ds(self.joined_ratings_tf_records,
-      self.feature_spec, batch_size=2048)
-    
-    shift_bits = 13
-    ubf, umbf = RetrieverAndRanker._init_rbloom(user_ids, ratings_ds, bits_shift=shift_bits)
-    
-    self.assertTrue(1 in ubf)
-    
-    #known user, movie rating pair
-    self.assertTrue((6040 << shift_bits)+858 in umbf)
-    
+      
   def test_retrieval(self):
     
     rr = RetrieverAndRanker(user_movie_saved_model_dir = self.user_movie_models_dir,
                             movies_path = self.movie_inputs, users_path=self.user_inputs,
-                            ratings_paths = self.joined_ratings_tf_records,
                             movies_pivot_path=self.movies_predictions_pivot,
-                            max_k= 1000, movies_batch_size=256, ratings_batch_size=256)
-    
-    self.assertTrue(rr.is_user_known(1))
-    self.assertTrue(rr.has_seen_movie(6040, 858))
+                            max_k= 1000, movies_batch_size=256)
     
     #who are the users similar to user_id=1
     user_inp = {'user_id': 5077, 'age':25}
@@ -94,7 +67,7 @@ class TestRetrievalAndRanker(unittest.TestCase):
     print(f'sim_users: {sim_users}')
     #1587, 2059, 5684, 1859, 4899, 5217, 3468, 2345, 3040
     
-    sim_movies = rr.get_movies_given_users(user_inp, top_k=9, remove_aready_seen=True)
+    sim_movies = rr.get_movies_given_users(user_inp, top_k=9)
     print(f'sim_movies: {sim_movies}')
     #3089, 1572, 3030, 1068, 2731, 326, 1759, 3134, 2575, 2940
     
@@ -104,6 +77,9 @@ class TestRetrievalAndRanker(unittest.TestCase):
     
     sim_movies = rr.get_movies_given_movies(movie_inp, top_k=9)
     print(f'sim_movies: {sim_movies}')
+    
+    cold_starts = rr.get_cold_start_movie_recommendations(10)
+    print(f'cold_starts: {cold_starts}')
     
   if __name__ == '__main__':
     unittest.main()
